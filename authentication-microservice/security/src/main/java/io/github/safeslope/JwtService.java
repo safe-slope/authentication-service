5e0dba4bb51423c6e07c42b82a4de0416dbce0c3
Andrej R <andrej.repic04@gmail.com>
2026-01-14 15:08:35 +0100

    Cleaned up unused imports in SecurityConfig and JwtService

--- authentication-microservice/security/src/main/java/io/github/safeslope/JwtService.java
+++ authentication-microservice/security/src/main/java/io/github/safeslope/JwtService.java
@@ -4,12 +4,8 @@ import io.github.safeslope.entities.User;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.io.Decoders;
-import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
-
 import java.security.Key;
-import java.security.PrivateKey;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
